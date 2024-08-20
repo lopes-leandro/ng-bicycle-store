0c5fd7658f6fb1f5cfdd742a2fee23baccd8dcd0
Leandro Lopes <lopesleandro.it@gmail.com>
2024-08-20 11:07:53 -0300

    style: :fire: redefinição do projeto
    Redefinida a página inicial do projeto.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-bicycle-store'`, () => {
+  it(`should have as title 'Bicycle Store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-bicycle-store');
+    expect(app.title).toEqual('Bicycle Store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-bicycle-store app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Bicycle Store app is running!');
   });
 });
